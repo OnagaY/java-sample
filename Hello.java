898bd47f219c5f31f669b6035bd7aa005068664d
OnagaY <kaihatu.yasutake.onaga@gmail.com>
2023-04-28 18:15:09 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+//class Hello{
+    //public static void main(String args[]){
+      //  System.out.println("こんにちは、私はJavaです。これはハードコーティングされています。");
+
+   // }
+//}
+
+class Hello {
+    public static void main(String args[]) {
+        String a = "aiueo";
+        int b = 3;
+        System.out.println( a + b );
+    }
+}
+
